740ef08d76a5fda5dbabf815376ec8a350e7aadf
SUBRAMANIAN S V <svsubramanian3600@gmail.com>
2023-12-20 22:07:53 +0530

    Basic App Server

---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+});
