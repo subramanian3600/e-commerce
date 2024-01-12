1c6ef554eaf9687c90a3f32f00811b06f3283020
SUBRAMANIAN S V <svsubramanian3600@gmail.com>
2024-01-12 18:31:56 +0530

    basic admin component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        title: 'Admin',
+        path: 'admin',
+        loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    }
+];
